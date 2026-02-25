a990da82352cfd7121151ea09dcd7c4c63453442
ayaki102 <minecraftowykoks420@gmail.com>
2026-02-25 19:16:37 +0100

    fuck vercel

--- api/responses/[id].js
+++ api/responses/[id].js
@@ -4,7 +4,7 @@
 
 import { getClient, initSchema } from "../_db.js";
 
-export const config = { runtime: "nodejs20.x" };
+export const config = { runtime: "nodejs" };
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
